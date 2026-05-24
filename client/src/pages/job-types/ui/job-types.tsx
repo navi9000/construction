29178640cdc5dc32b4a3ad8d3fe25da353c071c6
navi9000 <icdi7434@gmail.com>
2026-05-24 11:58:01 +0400

    job type scaffolding

--- client/src/pages/job-types/ui/job-types.tsx
+++ client/src/pages/job-types/ui/job-types.tsx
@@ -1,7 +1,69 @@
-import type { FC } from "react"
+import { Button, Flex, Table, TableColumnsType, Typography } from "antd"
+import { useState, type FC } from "react"
+import TableModal from "./table-modal"
+
+interface TableEntry {
+  key: string
+  job: string
+  unit: string
+}
+
+const dataSource: TableEntry[] = [
+  {
+    key: "1",
+    job: "Кладка перегородок",
+    unit: "м3",
+  },
+  {
+    key: "2",
+    job: "Монтаж опалубки",
+    unit: "м3",
+  },
+]
+
+const columns: TableColumnsType<TableEntry> = [
+  {
+    title: "Вид работ",
+    dataIndex: "job",
+    key: "job",
+  },
+  {
+    title: "Ед. изм.",
+    dataIndex: "unit",
+    key: "unit",
+  },
+  {
+    key: "buttons",
+    render: (_, record) => (
+      <Flex justify="end" gap="small">
+        <a>Редактировать</a>
+        <a>Удалить</a>
+      </Flex>
+    ),
+  },
+]
 
 const JobTypes: FC = () => {
-  return <div>Jobs</div>
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <main>
+      <Typography.Title>Список записей</Typography.Title>
+      <Flex justify="end">
+        <Button onClick={openModal}>Новый вид работ</Button>
+      </Flex>
+      <Table dataSource={dataSource} columns={columns} pagination={false} />
+      <TableModal isOpen={isModalOpen} close={closeModal} />
+    </main>
+  )
 }
 
 export default JobTypes
---
+++ client/src/pages/job-types/ui/table-modal.tsx
@@ -0,0 +1,37 @@
+import { Modal, Input, Form, DatePicker, Space, Select } from "antd"
+import { FC } from "react"
+
+interface TableModalProps {
+  isOpen: boolean
+  close: () => void
+}
+
+const TableModal: FC<TableModalProps> = ({ isOpen, close }) => {
+  const onSubmit = () => {
+    close()
+  }
+
+  const onCancel = () => {
+    close()
+  }
+
+  return (
+    <Modal
+      open={isOpen}
+      onOk={onSubmit}
+      onCancel={onCancel}
+      title="Новая запись"
+    >
+      <Form>
+        <Form.Item label="Вид работ">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Единица измерения">
+          <Select />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+export default TableModal
