471f363a8f798c611f48b926720745b3b7908610
navi9000 <icdi7434@gmail.com>
2026-05-24 11:10:45 +0400

    entry list scaffolding

--- client/src/pages/entry-list/ui/entry-list.tsx
+++ client/src/pages/entry-list/ui/entry-list.tsx
@@ -1,7 +1,89 @@
+import {
+  Button,
+  DatePicker,
+  Flex,
+  Table,
+  TableColumnsType,
+  Typography,
+} from "antd"
 import type { FC } from "react"
 
+interface TableEntry {
+  key: string
+  date: string
+  job: string
+  amount: number
+  unit: string
+  worker_name: string
+}
+
+const dataSource: TableEntry[] = [
+  {
+    key: "1",
+    date: "15.02.2025",
+    job: "Кладка перегородок",
+    amount: 24,
+    unit: "м3",
+    worker_name: "Иванов И.И.",
+  },
+  {
+    key: "2",
+    date: "15.02.2025",
+    job: "Монтаж опалубки",
+    amount: 24,
+    unit: "м3",
+    worker_name: "Иванов И.И.",
+  },
+]
+
+const columns: TableColumnsType<TableEntry> = [
+  {
+    title: "Дата",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "Вид работ",
+    dataIndex: "job",
+    key: "job",
+  },
+  {
+    title: "Объем работ",
+    dataIndex: "amount",
+    key: "amount",
+  },
+  {
+    title: "Ед. изм.",
+    dataIndex: "unit",
+    key: "unit",
+  },
+  {
+    title: "ФИО",
+    dataIndex: "worker_name",
+    key: "worker_name",
+  },
+  {
+    key: "buttons",
+    render: (_, record) => (
+      <Flex justify="end" gap="small">
+        <a>Редактировать</a>
+        <a>Удалить</a>
+      </Flex>
+    ),
+  },
+]
+
 const EntryList: FC = () => {
-  return <div>Entries</div>
+  return (
+    <main>
+      <Typography.Title>Список записей</Typography.Title>
+      <Flex justify="space-between">
+        <DatePicker />
+        <Button>Новая запись</Button>
+      </Flex>
+      <Table dataSource={dataSource} columns={columns} pagination={false} />
+    </main>
+  )
 }
 
 export default EntryList
--- client/src/shared/ui/layout/layout.tsx
+++ client/src/shared/ui/layout/layout.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react"
-import { Outlet, useLocation } from "react-router"
+import { Outlet } from "react-router"
 import { Flex } from "antd"
 import TextButton from "./text-button"
 
