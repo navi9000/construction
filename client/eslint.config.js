3ab1134653628d37942b8f1cc6ca2e253323b5e3
navi9000 <icdi7434@gmail.com>
2026-06-12 10:42:29 +0400

    remove boundaries plugin

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,12 +4,10 @@ import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "typescript-eslint"
 import { defineConfig, globalIgnores } from "eslint/config"
-import boundaries from "eslint-plugin-boundaries"
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
-    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,66 +18,6 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
-  {
-    plugins: { boundaries },
-    settings: {
-      "boundaries/root-path": "./src",
-      "import/resolver": {
-        typescript: {
-          alwaysTryTypes: true,
-          project: "./tsconfig.json",
-        },
-      },
-      "boundaries/elements": [
-        {
-          type: "app",
-          pattern: "app/*",
-        },
-        {
-          type: "entities",
-          pattern: "entities/*",
-        },
-        {
-          type: "pages",
-          pattern: "pages/*",
-        },
-        {
-          type: "shared",
-          pattern: "shared/*",
-        },
-        {
-          type: "widgets",
-          pattern: "widgets/*",
-        },
-      ],
-    },
-    rules: {
-      "boundaries/dependencies": [
-        "error",
-        {
-          default: "allow",
-          checkAllOrigins: true,
-          rules: [
-            {
-              dependency: {
-                source: [
-                  "@/app/*/**",
-                  "@/entities/*/**",
-                  "@/pages/*/**",
-                  "@/widgets/*/**",
-                ],
-              },
-              disallow: {
-                from: {
-                  type: "*",
-                },
-              },
-              message: "Import from the module public API instead.",
-            },
-          ],
-        },
-      ],
-    },
+    files: ["**/*.{ts,tsx}"],
   },
 ])
